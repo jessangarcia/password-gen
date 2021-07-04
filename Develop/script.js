c58991712f6671fe004756bced1c1f462443a7f1
jessan <jessenia.garcia998@gmail.com>
2021-07-04 15:49:34 -0600

    window prompts and pass confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,42 @@
-// User input variables: 
-var enter;
-var confirmNumber;
-var confirmCharacter;
-var confirmUppercase;
-var confirmLowercase;
-// Start Password variable values: 
-// Special characters 
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-// Numeric characters
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// Alphabetical characters
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Space is for the Uppercase conversion
-space = [];
-// Choices declared outside the if statement so they can be concatenated upon condition
-var choices;
-// converts letters to uppercase 
-var toUpper = function (x) {
-    return x.toUpperCase();
-};
-// creates a variable for uppercase conversion
-alpha2 = alpha.map(toUpper);
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Variable Declaration 
+var confirmLength = "";
+var confirmSymbol;
+var confirmNumber;
+var confirmUpperCase;
+var confirmLowerCase;
+
+//prompt to confirm number of characters
+function generatePassword(){
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  //if answer is outside parameters 
+  while(confirmLength <= 7) {
+    alert("Password length must be 8 characters long")
+  }
+
+  //confirm password characters
+  var confirmSymbol = confirm("Click OK to confirm if you would like to include special characters");
+  var confirmNumber = confirm("Click OK to confirm if you would like to include numeric characters");    
+  var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+  var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+  // if they say no to one of these
+  while(confirmUpperCase === false && confirmLowerCase === false && confirmSymbol === false && confirmNumber === false) {
+    alert("You must choose at least one option");
+    var confirmSymbol = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumber = confirm("Click OK to confirm if you would like to include numeric characters");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
