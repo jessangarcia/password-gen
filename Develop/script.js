fcc9158003d2cc76749bf3d0960aec92d070d7d2
jessan <jessenia.garcia998@gmail.com>
2021-07-04 16:08:27 -0600

    assigning characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Various Arrays 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -33,6 +33,22 @@ function generatePassword(){
     var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
   }
 }
+//assign the characters 
+var passwordCharacters = []
+
+if (confirmSymbol) {
+  passwordCharacters = passwordCharacters.concat(symbol)
+}
+if (confirmNumber) {
+  passwordCharacters = passwordCharacters.concat(number)
+}
+if (confirmLowerCase) {
+  passwordCharacters = passwordCharacters.concat(alphaLower)
+}
+if (confirmUpperCase) {
+  passwordCharacters = passwordCharacters.concat(alphaUpper)
+}
+console.log(passwordCharacters);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
